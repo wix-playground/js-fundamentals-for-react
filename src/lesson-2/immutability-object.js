5463030cf8763815e64b34e47938fb3970c2b371
Elad Bogomolny <eladbo@wix.com>
2020-07-23 10:05:21 +0300

    new lessons and tasks

--- src/lesson-2/immutability-object.js
+++ src/lesson-2/immutability-object.js
@@ -1,8 +1,17 @@
 const todoState = {
-
+    someId : {
+        task: 'Finish the tutorial',
+        isDone: false
+    },
+    someOtherId: {
+        task: 'Do 15 push ups',
+        isDone: false
+    }
 }
 
-
+export function removePropertyImmutably(state, key) {
+    return {}
+}
 
 // your code goes here:
 export function removeTodo(state, id) {
--- src/lesson-2/immutaility-array.js
+++ src/lesson-2/immutaility-array.js
@@ -1,7 +1,9 @@
-const todosArray = []
+const todosArray = [1, 2]
 
 
-
+export function pushImmutable(arr, val) {
+    return []
+}
 
 // your code goes here:
 export function deleteTodoId(state, id) {
